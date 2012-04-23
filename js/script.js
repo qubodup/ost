7f4fd84f156078988bbb189e41ebddd676ede93a
Thomas Glamsch (CruzR) <thomas-glamsch@gmx.de>
2012-04-23 02:08:34 +0200

    Items are now javascript-generated. This allows us to choose between different texture libraries.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,268 @@
-/* Author:
+/* Image database */
+Images = {
+    'animals': {
+        'src': 'img/animals.jpg',
+        'alt': 'Animal texture by p0ss',
+        'href': 'http://opengameart.org/content/woodland-animals-texture-pack'},
+    'bones': {
+        'src': 'img/bones.jpg',
+        'alt': 'Bones texture by p0ss',
+        'href': 'http://opengameart.org/content/bone-texture-pack'},
+    'bricks': {
+        'src': 'img/bricks.jpg',
+        'alt': 'Brick texture by Tiziana',
+        'href': 'http://opengameart.org/content/grey-stone-brick-floor-256px'},
+    'buildings': {
+        'src': 'img/buildings.jpg',
+        'alt': 'Building texture by Crack.com',
+        'href': 'http://opengameart.org/content/golgotha-textures-widopeakwindowswalljpg'},
+    'camouflage': {
+        'src': 'img/camouflage.jpg',
+        'alt': 'Camouflage by Skorpio',
+        'href': 'http://opengameart.org/content/camouflage'},
+    'concrete': {
+        'src': 'img/concrete.jpg',
+        'alt': 'Concrete texture by Sindwiller',
+        'href': 'http://opengameart.org/content/muddy-concrete-wall-02'},
+    'crate': {
+        'src': 'img/crate.jpg',
+        'alt': 'Crate by Luke.RUSTLTD',
+        'href': 'http://opengameart.org/content/3-crate-textures-w-bump-normal'},
+    'decals': {
+        'src': 'img/decals.jpg',
+        'alt': 'Filth texture by TRaK',
+        'href': 'http://opengameart.org/content/filth-texture-set-trakrustdecal1tga'},
+    'doors': {
+        'src': 'img/doors.jpg',
+        'alt': 'Door by Saroman',
+        'href': 'http://opengameart.org/content/medieval-wooden-door'},
+    'fabric': {
+        'src': 'img/fabric.jpg',
+        'alt': 'Fabric by Anna Paar',
+        'href': 'http://opengameart.org/content/fair-elven-stitches'},
+    'fire': {
+        'src': 'img/fire.jpg',
+        'alt': 'Fire by Lamoot',
+        'href': 'http://opengameart.org/content/lava-texture'},
+    'food': {
+        'src': 'img/food.jpg',
+        'alt': 'Apple by qubodup',
+        'href': 'http://opengameart.org/content/apple-for-uv-map'},
+    'grass': {
+        'src': 'img/grass.jpg',
+        'alt': 'Grass by Lamoot',
+        'href': 'http://opengameart.org/content/grass-texture'},
+    'ground': {
+        'src': 'img/ground.jpg',
+        'alt': 'Ground texture by qubodup',
+        'href': 'http://opengameart.org/content/burnt-coal-ashes-tiling-1024'},
+    'metal': {
+        'src': 'img/metal.jpg',
+        'alt': 'Metal by TRaK',
+        'href': 'http://opengameart.org/content/metal-plate-grey-dirty'},
+    'ornaments': {
+        'src': 'img/ornaments.jpg',
+        'alt': 'Ornament by n3b',
+        'href': 'http://opengameart.org/content/seamless-decorative-tile'},
+    'paper': {
+        'src': 'img/paper.jpg',
+        'alt': 'Paper by Luke.RUSTLtD',
+        'href': 'http://opengameart.org/content/large-parchment-texture'},
+    'road': {
+        'src': 'img/road.jpg',
+        'alt': 'Road by yughues',
+        'href': 'http://opengameart.org/content/tilling-texture-pack-27'},
+    'rust': {
+        'src': 'img/rust.jpg',
+        'alt': 'Rusted metal by p0ss',
+        'href': 'http://opengameart.org/content/rusted-metal-texture-pack'},
+    'roof': {
+        'src': 'img/roof.jpg',
+        'alt': 'Roof by Allan Zieser',
+        'href': 'http://www.burningwell.org/gallery2/v/textures/buildings/roofs/TT1_0285.JPG.html'},
+    'signs': {
+        'src': 'img/signs.jpg',
+        'alt': 'Sign by Rafal Konieczny',
+        'href': 'http://commons.wikimedia.org/wiki/File:Sign_eyewash.svg'},
+    'skies': {
+        'src': 'img/skies.jpg',
+        'alt': 'Sky by MiniBjorn',
+        'href': 'http://opengameart.org/content/cloudy-sky'},
+    'vegetation': {
+        'src': 'img/vegetation.jpg',
+        'alt': 'Cactus by qubodup',
+        'href': 'http://opengameart.org/content/cactus-sprite-texture-0'},
+    'vehicles': {
+        'src': 'img/vehicles.jpg',
+        'alt': 'Car underside by qubodup',
+        'href': 'http://opengameart.org/content/fiat-underside'},
+    'water': {
+        'src': 'img/water.jpg',
+        'alt': 'Water by Rudy Phelippeau',
+        'href': 'http://opengameart.org/content/cool-water-texture'},
+    'windows': {
+        'src': 'img/windows.jpg',
+        'alt': 'Window by Jaycee Barratt',
+        'href': 'http://www.burningwell.org/gallery2/v/textures/buildings/windowsHouses/window926.jpg.html'},
+    'wood': {
+        'src': 'img/wood.jpg',
+        'alt': 'Wood by Bart K.',
+        'href': 'http://opengameart.org/content/seamless-tiling-tree-bark-texture'},
+    'xrays': {
+        'src': 'img/xrays.jpg',
+        'alt': 'Hand X-Ray by Nevit Dilmen',
+        'href': 'http://commons.wikimedia.org/wiki/File:Medical_X-Ray_imaging_MQA06_nevit.jpg'}
+};
 
-*/
+/* Item database */
+Items = {
 
+    'burningwell': [
+        {
+            'desc': 'Roofing',
+            'href': 'http://www.burningwell.org/gallery2/v/textures/buildings/roofs/',
+            'image': 'roof'},
+        {
+            'desc': 'Windows',
+            'href': 'http://www.burningwell.org/gallery2/main.php?g2_view=search.SearchScan&amp;g2_form%5BformName%5D=search_SearchBlock&amp;g2_form%5BsearchCriteria%5D=window&amp;g2_form%5BuseDefaultSettings%5D=1&amp;g2_form%5Baction%5D%5Bsearch%5D=Search',
+            'image': 'windows'}
+    ],
 
+    'oga': [
+        {
+            'desc': 'Animals',
+            'href': 'http://opengameart.org/art-search-advanced?field_art_type_tid%5B%5D=14&amp;keys=animal',
+            'image': 'animals'},
+        {
+            'desc': 'Bones',
+            'href': 'http://opengameart.org/art-search-advanced?field_art_type_tid%5B%5D=14&amp;keys=bone',
+            'image': 'bones'},
+        {
+            'desc': 'Bricks',
+            'href': 'http://opengameart.org/art-search-advanced?field_art_type_tid%5B%5D=14&amp;keys=brick',
+            'image': 'bricks'},
+        {
+            'desc': 'Buildings',
+            'href': 'http://opengameart.org/art-search-advanced?field_art_type_tid%5B%5D=14&amp;keys=building',
+            'image': 'buildings'},
+        {
+            'desc': 'Camouflage',
+            'href': 'http://opengameart.org/art-search-advanced?field_art_type_tid%5B%5D=14&amp;keys=camouflage',
+            'image': 'camouflage'},
+        {
+            'desc': 'Concrete',
+            'href': 'http://opengameart.org/art-search-advanced?field_art_type_tid%5B%5D=14&amp;keys=concrete',
+            'image': 'concrete'},
+        {
+            'desc': 'Crate',
+            'href': 'http://opengameart.org/art-search-advanced?field_art_type_tid%5B%5D=14&amp;keys=crate',
+            'image': 'crate'},
+        {
+            'desc': 'Decals',
+            'href': 'http://opengameart.org/art-search-advanced?field_art_type_tid%5B%5D=14&amp;keys=decal',
+            'image': 'decals'},
+        {
+            'desc': 'Doors',
+            'href': 'http://opengameart.org/art-search-advanced?field_art_type_tid%5B%5D=14&amp;keys=door',
+            'image': 'doors'},
+        {
+            'desc': 'Fabric',
+            'href': 'http://opengameart.org/art-search-advanced?field_art_type_tid%5B%5D=14&amp;keys=fabric',
+            'image': 'fabric'},
+        {
+            'desc': 'Fire',
+            'href': 'http://opengameart.org/art-search-advanced?field_art_type_tid%5B%5D=14&amp;keys=fire',
+            'image': 'fire'},
+        {
+            'desc': 'Food',
+            'href': 'http://opengameart.org/art-search-advanced?field_art_type_tid%5B%5D=14&amp;keys=food',
+            'image': 'food'},
+        {
+            'desc': 'Grass',
+            'href': 'http://opengameart.org/art-search-advanced?field_art_type_tid%5B%5D=14&amp;keys=grass',
+            'image': 'grass'},
+        {
+            'desc': 'Ground',
+            'href': 'http://opengameart.org/art-search-advanced?field_art_type_tid%5B%5D=14&amp;keys=ground',
+            'image': 'ground'},
+        {
+            'desc': 'Metal',
+            'href': 'http://opengameart.org/art-search-advanced?field_art_type_tid%5B%5D=14&amp;keys=metal',
+            'image': 'metal'},
+        {
+            'desc': 'Ornaments',
+            'href': 'http://opengameart.org/art-search-advanced?field_art_type_tid%5B%5D=14&amp;keys=ornament',
+            'image': 'ornaments'},
+        {
+            'desc': 'Paper',
+            'href': 'http://opengameart.org/art-search-advanced?field_art_type_tid%5b%5d=14&amp;keys=paper',
+            'image': 'paper'},
+        {
+            'desc': 'Road',
+            'href': 'http://opengameart.org/art-search-advanced?field_art_type_tid%5B%5D=14&amp;keys=road',
+            'image': 'road'},
+        {
+            'desc': 'Rust',
+            'href': 'http://opengameart.org/art-search-advanced?field_art_type_tid%5B%5D=14&amp;keys=rust',
+            'image': 'rust'},
+        {
+            'desc': 'Skies',
+            'href': 'http://opengameart.org/art-search-advanced?field_art_type_tid%5B%5D=14&amp;keys=sky',
+            'image': 'skies'},
+        {
+            'desc': 'Vegetation',
+            'href': 'http://opengameart.org/art-search-advanced?field_art_type_tid%5B%5D=14&amp;keys=vegetation',
+            'image': 'vegetation'},
+        {
+            'desc': 'Vehicles',
+            'href': 'http://opengameart.org/art-search-advanced?field_art_type_tid%5B%5D=14&amp;keys=vehicle',
+            'image': 'vehicles'},
+        {
+            'desc': 'Water',
+            'href': 'http://opengameart.org/art-search-advanced?field_art_type_tid%5B%5D=14&amp;keys=water',
+            'image': 'water'},
+        {
+            'desc': 'Wood',
+            'href': 'http://opengameart.org/art-search-advanced?field_art_type_tid%5b%5d=14&amp;keys=wood',
+            'image': 'wood'}
+    ],
 
+    'wikimedia': [
+        {
+            'desc': 'Signs',
+            'href': 'http://commons.wikimedia.org/wiki/Category:Signs',
+            'image': 'signs'},
+        {
+            'desc': 'X-Ray Images',
+            'href': 'http://commons.wikimedia.org/wiki/Category:Medical_X-rays',
+            'image': 'xrays'}
+    ]
+};
 
+insert_items = function(source) {
+    var node = $('div#content > p:first');
+    for (var i = 0; i < Items[source].length; i++) {
+        node.after(function() {
+            /*console.log(i);/**/
+            var it = Items[source][i];
+            var img = Images[it.image];
+            var el = '<div class="item">\n'
+                + '  <a class="item" href="' + it.href + '">\n'
+                + '  <img src="' + img.src + '" alt="' + img.alt + '" />\n'
+                + '  <span>' + it.desc + '</span></a>\n'
+                + '  <a class="source" href="' + img.href + '">image source</a>\n'
+                + '</div>';
+            return el;
+        });
+        node = $('div#content > div.item:last');
+    }
+};
 
+$(document).ready(function() {
+    $('select#src').change(function() {
+        var source = $('select#src option:selected').get(0).value;
+        $('div#content > div.item').remove();
+        insert_items(source);
+    });
+    insert_items('oga');
+});
