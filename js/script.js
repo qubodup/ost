d0033a3edc8c75dce5003119675f8457b9d71253
Thomas Glamsch (CruzR) <thomas-glamsch@gmx.de>
2012-04-23 17:18:55 +0200

    Implemented source switching mechanism.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-/* Author:
+change_src = function() {
+    var src = $('select#src option:selected').get(0).value;
+    $('div.item.' + src).toggle(true);
+    $('div.item:not(.' + src + ')').toggle(false);
+};
 
-*/
-
-
-
-
-
+$(document).ready(function() {
+    $('select#src').change(change_src);
+    change_src();
+});
